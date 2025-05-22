72a77d118e9423f3e7720f8819a37bbb05134b1d
Monet <codem0n3t@gmail.com>
2025-05-22 17:08:22 -0400

    NavBar and page routing for parent and child components

--- src/components/AboutUs/AboutUs.jsx
+++ src/components/AboutUs/AboutUs.jsx
@@ -1,9 +1,19 @@
 import "./AboutUs.css";
+import { Link, Outlet } from "react-router-dom";
 
 function AboutUs() {
   return (
     <div className="about-us">
+      <ul className="links">
+        <li>
+          <Link path="site-history">Site History</Link>
+        </li>
+        <li>
+          <Link path="site-mission">Site Mission</Link>
+        </li>
+      </ul>
       <p>You can find out more information about our site here.</p>
+      <Outlet />
     </div>
   );
 }
